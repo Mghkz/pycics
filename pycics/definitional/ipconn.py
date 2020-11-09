77457060f0a23defbb7bc4ec2cf13f4268c39b33
ADMIN\jcn6el1 <jeroen.cornelis@colruytgroup.com>
2020-11-09 16:18:35 +0100

    delete ipconn definition

--- pycics/definitional/ipconn.py
+++ pycics/definitional/ipconn.py
@@ -26,15 +26,17 @@ def viewIPConnDefinition(host,port,authIn,context,scope,ipconnId):
         api_response2_alt = record.get('api_response2_alt')
 
     
-    if (api_response1 != "1024"):
+    if (api_response1 != "1024" and api_response1 != "1027"):
         raise Exception('View CSD Definition failed with response code {} ({}) and reason code {} ({})'.format(api_response1,api_response1_alt,api_response2,api_response2_alt))
+    elif api_response1 == "1027":
+        print("No IPCONN Definition found")
         
     for record in root.iter('{http://www.ibm.com/xmlns/prod/CICS/smw2int}cicsdefinitionipicconnection'):
         name    = record.get('name')
         description = record.get('description')
         print(name," => ",description)
 
-def defineIPConnDefinition(host,port,authIn,context,scope,ipconnName,csdgroup):
+def defineIPConnDefinition(host,port,authIn,context,scope,ipconnObject):
     
    # Build XML body for create action
 
@@ -72,29 +74,30 @@ def defineIPConnDefinition(host,port,authIn,context,scope,ipconnName,csdgroup):
     parameter  = ET.Element('parameter')
     parameter.set("name","CSD")
     
-    attributes = ET.Element('attributes')
-    attributes.set('NAME',ipconnName)
-    attributes.set('CSDGROUP',csdgroup)
-    attributes.set('APPLID',"")
-    attributes.set('NETWORKID',"")
-    attributes.set('HOST',"")
-    attributes.set('PORT',"")
-    attributes.set('TCPIPSERVICE',"")
-    attributes.set('RECEIVECOUNT',"")
-    attributes.set('SENDCOUNT',"")
-    attributes.set('QUEUELIMIT',"")
-    attributes.set('MAXQTIME',"")
-    attributes.set('MIRRORLIFE',"")
-    attributes.set('AUTOCONNECT',"")
-    attributes.set('INSERVICE',"")
-    attributes.set('HA',"NO")
-    attributes.set('SSL',"")
-    attributes.set('LINKAUTH',"")
-    attributes.set('USERAUTH',"")
-    attributes.set('XLNACTION',"KEEP")
+    
+    # attributes = ET.Element('attributes')
+    # attributes.set('NAME',ipconnName)
+    # attributes.set('CSDGROUP',csdgroup)
+    # attributes.set('APPLID',"")
+    # attributes.set('NETWORKID',"")
+    # attributes.set('HOST',"")
+    # attributes.set('PORT',"")
+    # attributes.set('TCPIPSERVICE',"")
+    # attributes.set('RECEIVECOUNT',"")
+    # attributes.set('SENDCOUNT',"")
+    # attributes.set('QUEUELIMIT',"")
+    # attributes.set('MAXQTIME',"")
+    # attributes.set('MIRRORLIFE',"")
+    # attributes.set('AUTOCONNECT',"")
+    # attributes.set('INSERVICE',"")
+    # attributes.set('HA',"NO")
+    # attributes.set('SSL',"")
+    # attributes.set('LINKAUTH',"")
+    # attributes.set('USERAUTH',"")
+    # attributes.set('XLNACTION',"KEEP")
 
     create.append(parameter)
-    create.append(attributes)
+    create.append(ipconnObject.toXML())
     request.append(create)
     reqBody = ET.tostring(request)
 
@@ -105,7 +108,7 @@ def defineIPConnDefinition(host,port,authIn,context,scope,ipconnName,csdgroup):
     resp = requests.post(url,reqBody,auth=authIn,verify=False)
     if resp.status_code != 200:
         # This means something went wrong.
-        raise Exception('POST /tasks/ {}'.format(resp.status_code))
+        raise Exception('POST /tasks/ {}\n{}'.format(resp.status_code,resp.content))
 
     content = resp.content
     root = ET.fromstring(content)
@@ -131,3 +134,36 @@ def defineIPConnDefinition(host,port,authIn,context,scope,ipconnName,csdgroup):
         # name    = record.get('name')
         # profile = record.get('profile')
         # print(name," => ",profile)
+
+def discardIpconnDefinition(host,port,authIn,context,scope,ipconnName,csdgroup):
+
+    uri = "/CICSSystemManagement/CICSDefinitionIPICConnection/"+context+"/"+scope+"?CRITERIA=NAME="+ipconnName+"&PARAMETER=CSDGROUP("+csdgroup+")"
+    url = "http://"+host+":"+port + uri
+
+
+    # 'verify=False' to disable ssl certificate verification
+    resp = requests.delete(url,auth=authIn,verify=False)
+    if resp.status_code != 200:
+        # This means something went wrong.
+        raise Exception('Delete {} {}'.format(uri,resp.status_code))
+
+    content = resp.content
+    root = ET.fromstring(content)
+    
+
+    for record in root.iter('{http://www.ibm.com/xmlns/prod/CICS/smw2int}resultsummary'):
+        api_response1 = record.get('api_response1')
+        api_response2 = record.get('api_response2')
+        api_response1_alt = record.get('api_response1_alt')
+        api_response2_alt = record.get('api_response2_alt')
+        #recordcount = record.get('recordcount')
+        #displayed_recordcount = record.get('displayed_recordcount')
+
+    # 1024 => Succesfully Discarded
+    # 1027 => No Records found so nothing to discard
+    if (api_response1 != "1024" and api_response1 != "1027" ):
+        raise Exception('Delete session definition failed with code {} ({}) - {} ({})'.format(api_response1,api_response1_alt,api_response2,api_response2_alt))
+    elif api_response1 == "1027":
+        print("No Session definition found for {} in group {}...".format(sessionName,csdgroup))
+    elif api_response1 == "1024":
+        print("ipconn",ipconnName,"definition discarded")
